e361a149e28c71212466f1d064568d3f439f3c5f
jdotc2 <jdotc2@gmail.com>
2019-11-24 10:56:45 -0500

    footer styling

--- amikka-frontend/src/components/footer/Footer.js
+++ amikka-frontend/src/components/footer/Footer.js
@@ -11,16 +11,13 @@ export default class Footer extends Component {
                     <Grid.Row><Grid.Column textAlign='center' color='blue' /></Grid.Row>
                     <Grid.Row><Grid.Column textAlign='center' color='blue' /></Grid.Row>
                     <Grid.Row centered color='blue' columns={3} divided>
-                        <Grid.Column width={7} textAlign='right' color='blue'>
+                        <Grid.Column width={7} textAlign='center' color='blue'>
                             Copyright © 2019 Amikka Tutoring
                         </Grid.Column>
-                        <Grid.Column width={2} textAlign='center' color='blue'>
-                            (305)-746-5982
-                        </Grid.Column>
-                        <Grid.Column width={7} textAlign='left' color='blue'>
-                            Info@amikkatutoring.com
-                        </Grid.Column>
                     </Grid.Row>
+                    <Grid.Row><Grid.Column textAlign='center' color='blue' /></Grid.Row>
+                    <Grid.Row><Grid.Column textAlign='center' color='blue' /></Grid.Row>
+                    <Grid.Row><Grid.Column textAlign='center' color='blue' /></Grid.Row>
                     <Grid.Row><Grid.Column textAlign='center' color='blue' /></Grid.Row>
                     <Grid.Row><Grid.Column textAlign='center' color='blue' /></Grid.Row>
                     <Grid.Row><Grid.Column textAlign='center' color='blue' /></Grid.Row>
